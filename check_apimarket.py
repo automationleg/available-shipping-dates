5f55621ced9451c982b1c85959787ccd7ef2a961
Krzysztof Ramuk <kramuk@pl.imshealth.com>
2020-04-10 11:44:13 +0200

    fix scp method

--- check_apimarket.py
+++ check_apimarket.py
@@ -131,8 +131,8 @@ def send_file_to_openhab(filename, hostname):
     from scp import SCPClient
 
     ssh = SSHClient()
-    ssh.load_system_host_keys()
-    ssh.connect(hostname)
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, username='vagrant')
 
     # SCPCLient takes a paramiko transport as an argument
     scp = SCPClient(ssh.get_transport())
